a125c5f262282ad08c692aaf2a2dbc9836288471
Jalen Eilert <jjeilert@ksu.edu>
2025-04-10 17:53:07 -0500

    situate files

---
+++ 3way-pthread/build/CMakeFiles/HW4_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW4_test.dir/include/pthread_main.c.o"
+  "CMakeFiles/HW4_test.dir/include/pthread_main.c.o.d"
+  "HW4_test"
+  "HW4_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HW4_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
